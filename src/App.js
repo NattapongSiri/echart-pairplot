af0a6ed483c7897a59d8223ff0290fff02712378
sathidporn <77266865+sathidporn@users.noreply.github.com>
2022-02-09 16:04:01 +0700

    Add import csv data and sensors selector

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {useCallback, useMemo, useState} from 'react'
+import Grid from '@material-ui/core/Grid'
 
 import { generateRandomSeries } from './utils/data_generator';
 import PairPlot from './components/PairPlot';
 import ClustersTable from './components/ClustersTable';
+import SensorsTable from './components/SensorsTable';
 
 const seriesConfigs = {
   "seriesA": {},
@@ -16,13 +18,13 @@ const defaultCluster = {
   color: "#00ff00"
 }
 function App() {
-  let series = useMemo(() => {
-    let temp = {}
-    for (let [key, value] of Object.entries(seriesConfigs)) {
-      temp[key] = generateRandomSeries(value)
-    }
-    return temp
-  }, [])
+  // let series = useMemo(() => {
+  //   let temp = {}
+  //   for (let [key, value] of Object.entries(seriesConfigs)) {
+  //     temp[key] = generateRandomSeries(value)
+  //   }
+  //   return temp
+  // }, [])
   const [clusters, setClusters] = useState([defaultCluster])
   const clustersChangeHandler = useCallback(clusters => {
     setClusters(clusters)
@@ -31,10 +33,26 @@ function App() {
   const activeClusterChangeHandler = useCallback(index => {
     setActiveClusterIndex(index)
   }, [setActiveClusterIndex])
+
+  // series from csv file
+  let [series, setSeries] = useState()
+  const onGetSeries = useCallback((data) => {
+    setSeries(data)
+  },[setSeries])
+
   return (
     <div className="App">
-      <PairPlot style={{width: '100%', tableLayout: 'fixed'}} series={series} activeClusterIndex={activeClusterIndex} clusters={clusters} />
-      <ClustersTable clusters={clusters} onChange={clustersChangeHandler} activeClusterIndex={activeClusterIndex} onActiveChange={activeClusterChangeHandler} />
+      <Grid item container lg={12} spacing={1}>
+        <Grid item lg={2}>
+          <SensorsTable onGetSeries={onGetSeries}></SensorsTable>
+          <ClustersTable clusters={clusters} onChange={clustersChangeHandler} activeClusterIndex={activeClusterIndex} onActiveChange={activeClusterChangeHandler} />
+        </Grid>
+        <Grid item lg={10}>
+          {series &&
+          <PairPlot style={{width: '100%', tableLayout: 'fixed'}} series={series} activeClusterIndex={activeClusterIndex} clusters={clusters} />
+          }
+        </Grid>
+      </Grid>
     </div>
   );
 }
